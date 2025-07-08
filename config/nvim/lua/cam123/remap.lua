5c89efe4d3b4d068176bb46a3e5401ce97cb069b
Cameron Reed <cameron@cam123.dev>
2025-07-07 19:16:48 -0600

    Add nvim keybind to jump between corresponding .c/.cpp files and their .h file

--- config/nvim/lua/cam123/remap.lua
+++ config/nvim/lua/cam123/remap.lua
@@ -13,3 +13,44 @@ end
 
 vim.keymap.set("n", "<leader>st", function() insert_text("θ") end)
 vim.keymap.set("n", "<leader>sp", function() insert_text("π") end)
+
+
+local function open_if_exists(filename)
+    if vim.fn.filereadable(filename) == 1 then
+        vim.cmd.edit(filename)
+        return 1
+    end
+
+    return 0
+end
+
+vim.keymap.set("n", "<leader>fh", function()
+    local filename = vim.api.nvim_buf_get_name(0)
+    local extension = filename:match("^.+%.([^%./\\]+)$")
+
+    if (extension == "h") then
+        local c_name = filename:gsub("%.h$", ".c")
+        local cpp_name = filename:gsub("%.h$", ".cpp")
+
+        if open_if_exists(c_name) == 1 then return end
+        c_name = c_name:gsub("/inc/", "/src/")
+        if open_if_exists(c_name) == 1 then return end
+        c_name = c_name:gsub("/include/", "/src/")
+        if open_if_exists(c_name) == 1 then return end
+
+        if open_if_exists(cpp_name) == 1 then return end
+        cpp_name = cpp_name:gsub("/inc/", "/src/")
+        if open_if_exists(cpp_name) == 1 then return end
+        cpp_name = cpp_name:gsub("/include/", "/src/")
+        if open_if_exists(cpp_name) == 1 then return end
+
+    elseif (extension == "c" or extension == "cpp") then
+        local header_name = filename:gsub("%.[^%./\\]+$", ".h")
+
+        if open_if_exists(header_name) == 1 then return end
+        header_name = header_name:gsub("/src/", "/inc/")
+        if open_if_exists(header_name) == 1 then return end
+        header_name = header_name:gsub("/inc/", "/include/")
+        if open_if_exists(header_name) == 1 then return end
+    end
+end)
