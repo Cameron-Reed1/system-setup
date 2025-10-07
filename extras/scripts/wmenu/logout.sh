78bff29e56e9d92e1e227b44ddb0f8d094a5c92d
Cameron Reed <cameron@cam123.dev>
2025-10-07 15:57:21 -0600

    Hyprland update

--- extras/scripts/wmenu/logout.sh
+++ extras/scripts/wmenu/logout.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 
-lines='-l 4'
-options='Log out\nShutdown\nReboot\nCancel'
+options='Log out\nLock Session\nShutdown\nReboot\nCancel'
 
-answer=$(echo -e "$options" | wmenu "$lines" -i -p 'Action:')
+answer=$(echo -e "$options" | wmenu -l "$(echo -e "$options" | wc -l)" -i -p 'Action:')
 
 case $answer in
 	'Log out')
 		hyprctl dispatch exit
 		;;
+    'Lock Session')
+        loginctl lock-session && hyprctl dispatch dpms off
+        ;;
 	'Shutdown')
 		systemctl poweroff
 		;;
